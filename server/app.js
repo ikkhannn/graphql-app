23d5b52ff561fa862a77a513c849b9670ba5c3db
imran <imrankhannn81@gmail.com>
2021-03-11 22:59:28 +0500

    graphql

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const app = express();
 
+
+
+app.get('/', function (req, res) {
+    console.log('hi');
+    res.send('hello world');
+  
+})
 
 app.listen(4000,()=>{
     console.log('now listening for requests on port 4000');
